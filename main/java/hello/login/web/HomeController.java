1af4b6b011de265fb37a159420dadee6d83f822e
layerdst <bccfcc@hanmail.net>
2022-10-22 23:28:51 +0900

    interceptor login

--- main/java/hello/login/web/HomeController.java
+++ main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -74,9 +75,20 @@ public class HomeController {
         return "/members/loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3Spring(
             @SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+
+        if(loginMember ==null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/members/loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model ){
 
         if(loginMember ==null){
             return "home";
